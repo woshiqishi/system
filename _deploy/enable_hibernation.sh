f007d19b7bc6b63cb37d587036843703596b962e
wasp <woshiqishi@proton.me>
2023-06-02 15:40:02 -0300

    enable_hibernation.sh: Edit sleep.conf

--- _deploy/enable_hibernation.sh
+++ _deploy/enable_hibernation.sh
@@ -54,8 +54,48 @@ replace_grub_line() {
   sudo sed -i "s/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"resume=UUID=$swap_uuid\"/" "$source_file"
   if [ $? -eq 0 ]; then
     echo "Line replaced successfully in $source_file"
+    echo "Running sudo grub-update"
+    sudo grub-update
+        if [ $? -eq 0 ]; then
+            echo "Grub successfully updated."
+        else
+            echo "Grub update failed."
+        fi
   else
     echo "Line replacement failed."
+  fi
+}
+
+# Check if a line starting with 'HibernateMode=' exists in /etc/systemd/sleep.conf
+check_hibernate_mode_line() {
+  local file="/etc/systemd/sleep.conf"
+
+  if grep -q '^HibernateMode=' "$file"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Replace or append the HibernateMode line in /etc/systemd/sleep.conf
+modify_hibernate_mode() {
+  local file="/etc/systemd/sleep.conf"
+  local line="HibernateMode=shutdown"
+
+  if check_hibernate_mode_line; then
+    sudo sed -i "s/^HibernateMode=.*/$line/" "$file"
+    if [ $? -eq 0 ]; then
+      echo "Line replaced successfully in $file"
+    else
+      echo "Line replacement failed."
+    fi
+  else
+    echo "$line" | sudo tee -a "$file" >/dev/null
+    if [ $? -eq 0 ]; then
+      echo "Line appended successfully to $file"
+    else
+      echo "Line append failed."
+    fi
   fi
 }
 
@@ -66,6 +106,7 @@ main() {
   print_swap_uuid "$swap_uuid"
   backup_grub
   replace_grub_line "$swap_uuid"
+  modify_hibernate_mode
 }
 
 # Run the script
