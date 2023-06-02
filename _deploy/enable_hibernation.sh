6fd0f1c272c62705adfb4ee9aa6ef9ac55bdcc59
wasp <woshiqishi@proton.me>
2023-06-02 15:15:50 -0300

    enable_hibernation.sh: Add grub backup

--- _deploy/enable_hibernation.sh
+++ _deploy/enable_hibernation.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 
 # Get the UUID of the swap partition
-swap_uuid=$(blkid -o value -s UUID -t TYPE=swap)
+get_swap_uuid() {
+  blkid -o value -s UUID -t TYPE=swap
+}
 
 # Check if the swap UUID is empty
-if [ -z "$swap_uuid" ]; then
-  echo "Swap partition not found or not configured."
-  exit 1
-fi
+check_swap_uuid() {
+  local swap_uuid=$1
+
+  if [ -z "$swap_uuid" ]; then
+    echo "Swap partition not found or not configured."
+    exit 1
+  fi
+}
 
 # Print the UUID of the swap partition
-echo "UUID of the swap partition: $swap_uuid"
+print_swap_uuid() {
+  local swap_uuid=$1
+
+  echo "UUID of the swap partition: $swap_uuid"
+}
+
+# Backup /etc/default/grub
+backup_grub() {
+  local source_file="/etc/default/grub"
+  local backup_file="/etc/default/grub.bak"
+
+  if [ ! -f "$source_file" ]; then
+    echo "Source file $source_file not found."
+    exit 1
+  fi
+
+  cp "$source_file" "$backup_file"
+  if [ $? -eq 0 ]; then
+    echo "Backup created successfully: $backup_file"
+  else
+    echo "Backup creation failed."
+  fi
+}
+
+# Main function
+main() {
+  swap_uuid=$(get_swap_uuid)
+  check_swap_uuid "$swap_uuid"
+  print_swap_uuid "$swap_uuid"
+  backup_grub
+}
+
+# Run the script
+main
