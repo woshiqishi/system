c1e908b58ae527f67ce463b703eed07e0276b586
wasp <woshiqishi@proton.me>
2023-06-02 03:17:59 -0300

    Update 'code' directory

--- _deploy/symlink_code_settings.sh
+++ _deploy/symlink_code_settings.sh
@@ -2,7 +2,7 @@
 
 if command -v code >/dev/null 2>&1; then
     # 'code' is installed
-    if ln -sf "$HOME/system/dotfiles/code/settings.json" "$HOME/.config/Code - OSS/User/settings.json"; then
+    if ln -sf "$HOME/system/dotfiles/code/settings.json" "$HOME/.config/Code/User/settings.json"; then
         echo "Code symlink created successfully."
     else
         echo "Code symlink was not created."
