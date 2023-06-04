6419861ee7579c66b5c497a47246e964afd2f395
wasp <woshiqishi@proton.me>
2023-06-04 07:22:12 -0300

    @install_zsh.sh: Improve 'make_default_shell()'

--- _deploy/@install_zsh.sh
+++ _deploy/@install_zsh.sh
@@ -2,7 +2,7 @@
 
 # Function to check Zsh installation and execute in test mode
 check_zsh_installation() {
-    if ! command -v zsh &> /dev/null; then
+    if ! command -v zsh &>/dev/null; then
         if [ "$1" = "--test" ]; then
             echo "Test mode: Zsh is not installed."
             echo "Command to be executed: sudo apt install zsh"
@@ -13,7 +13,7 @@ check_zsh_installation() {
             # Set Zsh as the default shell
             if [ "$SHELL" != "$(which zsh)" ]; then
                 echo "Setting Zsh as the default shell..."
-                sudo chsh -s "$(which zsh)"
+                chsh -s "$(which zsh)"
             fi
         fi
     else
@@ -22,11 +22,29 @@ check_zsh_installation() {
             echo "Command to be executed: <none>"
         else
             echo "Zsh is already installed."
+        fi
+    fi
+}
 
-            # Set Zsh as the default shell
-            if [ "$SHELL" != "$(which zsh)" ]; then
-                echo "Setting Zsh as the default shell..."
-                sudo chsh -s "$(which zsh)"
+make_default_shell() {
+    # Set Zsh as the default shell
+    if [ "$SHELL" != "$(which zsh)" ]; then
+        if [ "$1" = "--test" ]; then
+            echo "Test mode: Zsh is not your default shell."
+            echo "Test mode: Setting Zsh as the default shell...."
+            echo 'Command to be executed: chsh -s "$(which zsh)'
+            echo "Test mode: Done! do you want to log out? (y/n)"
+            read -r choice
+            if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+                echo 'Command to be executed: gnome-session-quit'
+            fi
+        else
+            echo "Setting Zsh as the default shell..."
+            chsh -s "$(which zsh)"
+            echo "Done! Do you want to log out? (y/n)"
+            read -r choice
+            if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+                gnome-session-quit
             fi
         fi
     fi
@@ -36,6 +54,11 @@ check_zsh_installation() {
 main() {
     # Call additional functions and logic here as needed
     check_zsh_installation "$@"
+    make_default_shell "$@"
+    # symlink_zshrc "$@"
+    # install_ohmyzsh "$@"
+    # install_ohmyzsh_plugins "$@"
+    
 }
 
 # Execute main function
