49688035ec3d8a06b894edd8e0ff3301671d61f0
wasp <woshiqishi@proton.me>
2023-06-05 00:25:44 -0300

    @install_zsh.sh: Switch to antigen

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 
 - [ ] Add custom bookmarks.
 - [ ] Automaticlly enable addons.
+- [ ] Customize or disable /chrome
+
+## enable_hibernation.sh
+
+- [ ] It's not hibernating.
+
+## @install_zsh.sh
+
+- [ ] Install antigen
+- [ ] Install spaceship (antigen theme spaceship-prompt/spaceship-prompt) or starship
+- [ ] Install autosuggestions (antigen bundle zsh-users/zsh-autosuggestions)
+- [ ] Install syntax highlighting (antigen bundle zsh-users/zsh-syntax-highlighting)
--- _deploy/@install_zsh.sh
+++ _deploy/@install_zsh.sh
@@ -3,51 +3,58 @@
 # Function to check Zsh installation and execute in test mode
 check_zsh_installation() {
     if ! command -v zsh &>/dev/null; then
-        if [ "$1" = "--test" ]; then
-            echo "Test mode: Zsh is not installed."
-            echo "Command to be executed: sudo apt install zsh"
-        else
-            echo "Zsh is not installed. Installing Zsh..."
-            sudo apt install zsh
+        echo "Zsh is not installed. Installing Zsh..."
+        sudo apt install zsh
 
-            # Set Zsh as the default shell
-            if [ "$SHELL" != "$(which zsh)" ]; then
-                echo "Setting Zsh as the default shell..."
-                chsh -s "$(which zsh)"
+        # Set Zsh as the default shell
+        if [ "$SHELL" != "$(which zsh)" ]; then
+            echo "Setting Zsh as the default shell..."
+            if chsh -s "$(which zsh)"; then
+                echo "Done! restart your terminal."
             fi
         fi
     else
-        if [ "$1" = "--test" ]; then
-            echo "Test mode: Zsh is already installed."
-            echo "Command to be executed: <none>"
-        else
-            echo "Zsh is already installed."
-        fi
+        echo "Zsh is already installed."
     fi
 }
 
 make_default_shell() {
     # Set Zsh as the default shell
     if [ "$SHELL" != "$(which zsh)" ]; then
-        if [ "$1" = "--test" ]; then
-            echo "Test mode: Zsh is not your default shell."
-            echo "Test mode: Setting Zsh as the default shell...."
-            echo 'Command to be executed: chsh -s "$(which zsh)'
-            echo "Test mode: Done! do you want to log out? (y/n)"
-            read -r choice
-            if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-                echo 'Command to be executed: gnome-session-quit'
-            fi
-        else
-            echo "Setting Zsh as the default shell..."
-            chsh -s "$(which zsh)"
-            echo "Done! Do you want to log out? (y/n)"
-            read -r choice
-            if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-                gnome-session-quit
-            fi
+        echo "Setting Zsh as the default shell..."
+        chsh -s "$(which zsh)"
+        echo "Done! Do you want to log out? (y/n)"
+        read -r choice
+        if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+            gnome-session-quit
         fi
     fi
+}
+
+symlink_zshrc() {
+    local zshrc_directory="$HOME/system/dotfiles/zsh/.zshrc"
+
+    if ln -sf $zshrc_directory $HOME; then
+        echo ".zshrc symlink successfully created"
+    else
+        echo "ERROR: .zshrc symlink not created"
+    fi
+}
+
+install_antigen() {
+    # Install antigen
+    if curl -L git.io/antigen > "$HOME/system/dotfiles/zsh/antigen.zsh"; then
+        echo "Antigen was successfully installed."
+    else
+        echo "ERROR: antigen was not installed correctly!"
+    fi
+}
+
+install_plugins() {
+    # local spaceship-prompt=
+    # local zsh-autosuggestions=
+    # local zsh-syntax-highlighting=
+    echo "nothing"
 }
 
 # Main function
@@ -55,10 +62,9 @@ main() {
     # Call additional functions and logic here as needed
     check_zsh_installation "$@"
     make_default_shell "$@"
-    # symlink_zshrc "$@"
-    # install_ohmyzsh "$@"
-    # install_ohmyzsh_plugins "$@"
-    
+    symlink_zshrc "$@"
+    install_antigen "$@"
+    #install_ohmyzsh_plugins "$@"
 }
 
 # Execute main function
