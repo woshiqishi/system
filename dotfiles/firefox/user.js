5a0460a308234e745c409b170ef12531143b1bf2
wasp <woshiqishi@proton.me>
2023-06-06 08:42:18 -0300

    firefox: Disable smooth scrolling

--- dotfiles/firefox/user.js
+++ dotfiles/firefox/user.js
@@ -21,6 +21,8 @@ user_pref("dom.security.https_only_mode_ever_enabled", true);
 user_pref("doh-rollout.disable-heuristics", true);
 user_pref("network.trr.mode", 3);
 user_pref("browser.startup.page", 3);
+user_pref("general.smoothScroll", false);
+user_pref("browser.aboutConfig.showWarning", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"jid1-om7ejgwa1u8akg_jetpack-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_74145f27-f039-47ce-a470-a662b129930a_-browser-action\",\"addon_darkreader_org-browser-action\",\"simple-translate_sienori-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\",\"developer-button\",\"unified-extensions-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"profiler-button\",\"jid1-om7ejgwa1u8akg_jetpack-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_74145f27-f039-47ce-a470-a662b129930a_-browser-action\",\"addon_darkreader_org-browser-action\",\"simple-translate_sienori-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":19,\"newElementCount\":3}");
